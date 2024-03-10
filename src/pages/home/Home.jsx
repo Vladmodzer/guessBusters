5cb9f2c4fb8ce2d59932131c3c050e4173f41044
EugeneKolom <luxevgeniy@gmail.com>
2024-03-10 23:36:28 +0100

    feat: font size base and finish sidebar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,9 +7,9 @@ function Home() {
     <div className={styles.main_wrapper}>
       <p> MainPage</p>
       <Link to="/registration">to the registration</Link>
-      <Sidebar/>
       <p>link</p>
       <Link to="/rules">to the rule</Link>
+      <Sidebar/>
     </div>
   );
 }
